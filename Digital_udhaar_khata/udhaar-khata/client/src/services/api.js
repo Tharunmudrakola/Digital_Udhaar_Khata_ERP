99238a2d7fde8a859080f43f5dffadc4a2745079
Tharun <tharunmudrakola240@gmail.com>
2026-05-29 11:46:11 +0530

    Fix API URL

--- Digital_udhaar_khata/udhaar-khata/client/src/services/api.js
+++ Digital_udhaar_khata/udhaar-khata/client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://digital-udhaar-khata-erp.onrender.com"
+    baseURL: "https://digital-udhaar-khata-erp.onrender.com/api"
 });
 
 export default API;
