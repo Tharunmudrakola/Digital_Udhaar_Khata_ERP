5fd44558ae375b965aeb34697255ee202cdd5782
Tharun <tharunmudrakola240@gmail.com>
2026-05-30 10:29:06 +0530

    Fix bills API

--- Digital_udhaar_khata/udhaar-khata/client/src/services/api.js
+++ Digital_udhaar_khata/udhaar-khata/client/src/services/api.js
@@ -1,7 +1,21 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://digital-udhaar-khata-erp.onrender.com/api"
+  baseURL: "https://digital-udhaar-khata-erp.onrender.com/api",
 });
+
+// Automatically attach token to every request
+API.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
